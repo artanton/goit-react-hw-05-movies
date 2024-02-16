d3f8740c8e82bdf3eacb1d1e11a4069e014c9d69
artanton <127799680+artanton@users.noreply.github.com>
2024-02-16 23:42:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,8 +22,9 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-      </Route>
+      
       <Route path="*" element={<HomePage />} />
+      </Route>  
     </Routes>
   );
 };
